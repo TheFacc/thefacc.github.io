209e7773e9e9839bb140eb7ee6343606eba9bc93
TheFacc <imthefacc@gmail.com>
2021-05-28 01:11:55 +0200

    create and include Card component

---
+++ components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="img" :style="{ 'background-image': `url(${image})` }"></div>
+    <h3>{{ title }}</h3>
+    <p>{{ summary }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, default: () => '' },
+    image: { type: String, default: () => '' },
+    summary: { type: String, default: () => '' },
+  },
+}
+</script>
+
+<style scoped>
+.card,
+.card .img {
+  overflow: hidden;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.card {
+  background: #eeeeee;
+  padding: 20px 10px;
+  cursor: pointer;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+.card:hover {
+  transform: scale(1.03);
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+.card:hover .img {
+  /* transform: scale(1.12); */
+}
+h3 {
+  margin-bottom: 10px;
+}
+
+.img {
+  width: 100%;
+  height: 200px;
+  max-width: 600px;
+  margin: auto;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin-bottom: 20px;
+}
+</style>
--- pages/areas/_id.vue
+++ pages/areas/_id.vue
@@ -1,17 +1,31 @@
 <template>
   <main>
     <item-intro :item="item"></item-intro>
-    <section class="container">
+    <div class="container">
       <p>this is area {{ $route.params.id }}</p>
-    </section>
+      <section class="card-grid">
+        <div
+          v-for="(card, cardIndex) of cards"
+          :key="'card-' + cardIndex"
+          @click="goTo(`/blog/${article.id}`)"
+        >
+          <card
+            :title="card.title"
+            :summary="card.summary"
+            :image="card.image"
+          ></card>
+        </div>
+      </section>
+    </div>
   </main>
 </template>
 
 <script>
 import ItemIntro from '~/components/ItemIntro.vue'
+import Card from '~/components/Card.vue'
 
 export default {
-  components: { ItemIntro },
+  components: { ItemIntro, Card },
   asyncData({ $axios, route }) {
     // const { id } = route.params
     // const { data } = await $axios.get(
@@ -23,9 +37,60 @@ export default {
     // }
   },
   data() {
-    return { item: { title: 'itit', intro: 'itintro' } } // to be retrieved from db based on type(area,product) and route.params.id
+    return {
+      item: { title: 'item title', intro: 'some intro' },
+      cards: [
+        {
+          title: 'Product 1',
+          summary: 'Some area description',
+          image:
+            'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        },
+        {
+          title: 'Product 2',
+          summary: 'Some area description',
+          image:
+            'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        },
+        {
+          title: 'Product 3',
+          summary: 'Some area description',
+          image:
+            'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        },
+        {
+          title: 'Product 4',
+          summary: 'Some area description',
+          image:
+            'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        },
+        {
+          title: 'Product 5',
+          summary: 'Some area description',
+          image:
+            'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
+        },
+      ],
+    } // to be retrieved from db based on type(area,product) and route.params.id
   },
 }
 </script>
 
-<style></style>
+<style>
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(4, calc(100% / 4));
+  grid-gap: 10px;
+  margin-top: 40px;
+}
+@media screen and (max-width: 768px) {
+  .card-grid {
+    grid-template-columns: repeat(3, calc(100% / 3));
+  }
+}
+@media screen and (max-width: 600px) {
+  .card-grid {
+    grid-template-columns: repeat(2, calc(100% / 2));
+  }
+}
+</style>
