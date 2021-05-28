8aba5259042f5c5ac152b5f44320588114d88437
TheFacc <imthefacc@gmail.com>
2021-05-29 00:48:00 +0200

    create CardGrid component to embed <card> and better manage wrap and shapes

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="card">
-    <div class="img" :style="{ 'background-image': `url(${image})` }"></div>
+  <div class="card" ref="thisCard">
+    <div
+      class="img"
+      :style="{ 'background-image': `url(${image})` }"
+      ref="thisCardImg"
+    ></div>
     <h3>{{ title }}</h3>
     <p>{{ summary }}</p>
   </div>
@@ -13,36 +17,33 @@ export default {
     image: { type: String, default: () => '' },
     summary: { type: String, default: () => '' },
     shape: { type: String, default: () => '' },
+    indent: { type: Boolean, default: () => false },
   },
   mounted() {
-    const cards = document.querySelectorAll('.card-grid .card')
-    const cardImgs = document.querySelectorAll('.card-grid .card .img')
+    // style based on shape
+    // const card = this.$refs.thisCard
+    // const cardImg = this.$refs.thisCardImg
+    // if (this.shape === 'rectangle') {
+    //   card.style.padding = '10px 10px'
+    //   cardImg.style.height = '150px'
+    // } else if (this.shape === 'circle') {
+    //   card.style.borderRadius = '150px'
+    //   card.style.width = '200px'
+    //   card.style.height = '200px'
+    //   card.style.flex = '0 0 calc(30% - 20px)'
+    //   cardImg.style.height = '120px'
+    //   // offset second row of circles for aesthetic purpose
+    //   // cons: this only works well for 3-item rows and for 1,2,3 or 5 elements
+    //   if (this.indent) {
+    //     // card.style.marginLeft = '50%'
+    //     card.style.marginTop = '-20px'
+    //   }
+    // }
+
     if (this.shape === 'rectangle') {
-      cards.forEach((card) => {
-        card.style.padding = '20px 10px'
-      })
-      cardImgs.forEach((img) => {
-        img.style.height = '150px'
-      })
+      this.$refs.thisCard.classList.add('card-rectangle')
     } else if (this.shape === 'circle') {
-      cards.forEach((card) => {
-        card.style.borderRadius = '150px'
-        card.style.height = '200px'
-        card.style.width = '200px'
-      })
-      cardImgs.forEach((img) => {
-        img.style.height = '120px'
-      })
-      const grid = document.querySelector('.card-grid')
-      grid.style.gridTemplateColumns = 'repeat(3, calc(100% / 3))'
-      grid.style.gridGap = '0'
-      const lowCircles = document.querySelectorAll(
-        '.card-grid :nth-child(n+4) .card'
-      ) // requires displaying 3 items per row (always with circles)
-      lowCircles.forEach((lowCircle) => {
-        lowCircle.style.marginLeft = '50%'
-        lowCircle.style.marginTop = '-20px'
-      })
+      this.$refs.thisCard.classList.add('card-circle')
     }
   },
 }
@@ -55,22 +56,79 @@ export default {
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 }
 .card {
-  background: #eeeeee;
+  background: white;
+  color: #222;
   /* padding: 20px 10px; */
   cursor: pointer;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  margin: 5px;
+  /* box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23); */
 }
 .card:hover {
   transform: scale(1.03);
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
-.card:hover .img {
-  /* transform: scale(1.12); */
+.card:hover .img,
+.card:hover h3 {
+  margin-bottom: 0;
 }
+/* ::after (read more) */
+.card::after {
+  content: '➕';
+  position: absolute;
+  opacity: 0;
+  transform: translateY(15px) rotate(90deg);
+  transition: 0.2s;
+}
+.card:hover::after {
+  opacity: 1;
+  transform: translateY(0px);
+}
+/* card rectangle */
+.card.card-rectangle {
+  padding: 10px 10px;
+}
+.card.card-rectangle .img {
+  height: 150px;
+}
+.dark .card.card-rectangle {
+  background: transparent;
+  color: white;
+}
+/* card circle */
+.card.card-circle {
+  border-radius: 150px;
+  /* width: 200px;
+  height: 200px; */
+  /* flex: 0 0 calc(30% - 20px); */
+}
+.card.card-circle .img {
+  height: 120px;
+}
+/* card wrapping */
+@media screen and (min-width: 768px) {
+  .card.card-rectangle {
+    width: 22%;
+  }
+  .card.card-circle {
+    width: 30%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .card.card-rectangle,
+  .card.card-circle {
+    width: 30%;
+  }
+}
+@media screen and (max-width: 600px) {
+  .card.card-rectangle,
+  .card.card-circle {
+    width: 45%;
+  }
+}
+/* misc */
 h3 {
   margin-bottom: 10px;
 }
-
 .img {
   width: 100%;
   max-width: 600px;
---
+++ components/CardGrid.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card-grid" ref="thisGrid">
+    <card
+      v-for="(card, cardIndex) of cards"
+      :key="'card-' + cardIndex"
+      :title="card.title"
+      :summary="card.summary"
+      :image="card.image"
+      :shape="shape"
+      :indent="cardIndex > 2"
+      @click="goTo(`/blog/${article.id}`)"
+    ></card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cards: { type: Array, default: () => [] },
+    shape: { type: String, default: () => '' },
+  },
+  mounted() {
+    // if (this.shape === 'circle') {
+    //   const grid = this.$refs.thisGrid
+    //   grid.style.gridTemplateColumns = 'repeat(3, calc(100% / 3))'
+    //   grid.style.gridGap = '0'
+    // }
+  },
+}
+</script>
+
+<style scoped>
+.card-grid {
+  /* display: grid;
+  grid-template-columns: repeat(4, calc(100% / 4));
+  grid-gap: 10px; */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+/* .card-grid > div {
+  display: flex;
+  justify-content: center;
+} */
+/* @media screen and (max-width: 768px) {
+  .card-grid {
+    grid-template-columns: repeat(3, calc(100% / 3));
+  }
+}
+@media screen and (max-width: 600px) {
+  .card-grid {
+    grid-template-columns: repeat(2, calc(100% / 2));
+  }
+} */
+</style>
