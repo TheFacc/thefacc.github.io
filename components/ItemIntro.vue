77c1e05668c29e1158c5121b24ae08a64ba2f695
TheFacc <imthefacc@gmail.com>
2021-06-03 09:17:52 +0200

    svg: pass text + item to activate on load / + add chroma.js color management

--- components/ItemIntro.vue
+++ components/ItemIntro.vue
@@ -12,6 +12,8 @@
         :main-cx="mainCx"
         :main-cy="mainCy"
         :main-r="mainR"
+        :activate-item="$route.params.id - 1"
+        :text="['Area', 'switcher']"
         spacing="left"
         @itemClicked="onClickChild"
       ></circle-svg>
@@ -20,6 +22,8 @@
 </template>
 
 <script>
+const chroma = require('chroma-js')
+
 export default {
   props: {
     item: {
@@ -33,25 +37,25 @@ export default {
     return {
       areas: [
         {
-          name: 'Cloud computing',
+          // name: 'Cloud computing',
           path: '/areas/1',
           color: '#F44336',
           icon: require('~/assets/icons/area-cloud-computing.svg?raw'),
         },
         {
-          name: 'Analytics',
+          // name: 'Analytics',
           path: '/areas/2',
           color: '#4CAF50',
           icon: require('~/assets/icons/area-analytics-2.svg?raw'),
         },
         {
-          name: 'Machine learning',
+          // name: 'Machine learning',
           path: '/areas/3',
           color: '#FFC107',
           icon: require('~/assets/icons/area-machine-learning.svg?raw'),
         },
         {
-          name: 'Blockchain',
+          // name: 'Blockchain',
           path: '/areas/4',
           color: '#00BCD4',
           icon: require('~/assets/icons/area-blockchain.svg?raw'),
@@ -60,14 +64,28 @@ export default {
       mainCx: 450,
       mainCy: 250,
       mainR: 150,
-      vbox: '-50 0 680 500',
+      vbox: '240 0 400 500',
       // item: { title: 'tit', intro: 'dxx' }, // to be later retrieved from db
     }
   },
   mounted() {
     // this.$nuxt.$emit('item', this.item) // pass retrieved item to item-layout
     // set bg color based on current area: (also highlight current area in circle)
-    // document.querySelector('.item-intro').style.background = 'linear-gradient(130deg,#ff7a18,#af002d 41.07%,#319197 76.05%)'
+    const color0 = chroma(this.item.color).set('hsl.l', 0.2).hex()
+    // const color1 = chroma(this.item.color)
+    //   .set('hsl.h', 20)
+    //   .set('hsl.s', 0.5)
+    //   .set('hsl.l', 0.3)
+    //   .hex()
+    const color2 = chroma(this.item.color)
+      // .set('hsl.h', 0)
+      .set('hsl.s', 0.8)
+      .set('hsl.l', 0.1)
+      .hex()
+    document.querySelector(
+      '.item-intro'
+    ).style.background = `linear-gradient(130deg, ${color0} 50%, ${color2} 50.05%)`
+    // ).style.background = `linear-gradient(130deg, #ff7a18,#af002d 41%, #319197 75%)` // original
   },
   methods: {
     goTo(path) {
@@ -89,12 +107,16 @@ export default {
   color: white;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
+  /* box-shadow: inset 0 0 5em -2em #000; */
 }
 .intro-left,
 .intro-right {
   position: relative;
+}
+.intro-left {
+  width: 25%;
 }
 .item-intro svg {
   height: 300px;
