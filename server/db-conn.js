f158a9b0b5b4f55e83a02d8d99c6805a7a0113e8
TheFacc <imthefacc@gmail.com>
2021-06-03 14:36:36 +0200

    improved structure and added more api to get it

--- server/db-conn.js
+++ server/db-conn.js
@@ -7,7 +7,11 @@ const { Sequelize, DataTypes } = require('sequelize')
 // Production
 const pg = require('pg')
 pg.defaults.ssl = true
+// HEROKU:
 const db = new Sequelize(process.env.DATABASE_URL, {
+  // LOCAL:
+  // const db = new Sequelize( 'longlink',
+  //   {
   ssl: true,
   dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 })
@@ -22,11 +26,11 @@ function defineDatabaseStructure() {
     {
       name: DataTypes.STRING,
       subtitle: DataTypes.STRING,
-      shortIntro: DataTypes.TEXT,
-      introduction: DataTypes.TEXT,
-
-      path: DataTypes.STRING,
+      introShort: DataTypes.TEXT,
+      intro: DataTypes.TEXT,
       color: DataTypes.STRING,
+      icon: DataTypes.STRING,
+      image: DataTypes.STRING,
     },
     {
       underscored: true,
@@ -89,86 +93,24 @@ function defineDatabaseStructure() {
 
   // Creating the 1 -> N association between Area and People
   Area.hasMany(Person)
-  db._tables = {
-    Area,
-    Person,
-  }
-
   // Creating the 1 -> N association between Area and Products
   Area.hasMany(Product)
-  db._tables = {
-    Area,
-    Product,
-  }
   // Creating the 1 -> N association between Products and UseCase
   Product.hasMany(UseCase)
-  db._tables = {
-    UseCase,
-    Product,
-  }
   // Creating the 1 -> 1 association between Products and Person
   // the product has one referent
   Person.hasOne(Product, { foreignKey: 'referentId' })
-  db._tables = {
-    Product,
-    Person,
-  }
-
   // Creating the 1 -> 1 association between Area and Person
   // the area has one manager
   Area.hasOne(Person, { foreignKey: 'managerOf' })
   db._tables = {
     Area,
     Person,
+    Product,
+    UseCase,
   }
 }
 
-/**
- * Function to insert some fake info in the database
- */
-// NOTA problema: come associo le persone all'area?
-async function insertFakeData() {
-  const { Person, Area, Product } = db._tables
-  // Create the first Person
-  const person1 = await Person.create({
-    name: 'Bob Minion',
-    role: 'manager',
-    image_src: '~./assets/people/Bob_Minion.jpg',
-  })
-
-  // create the second person
-  const person2 = await Person.create({
-    name: 'Wllie Space',
-    role: 'referent',
-    image_src: '~./assets/people/Wallie_Space.jpg',
-  })
-
-  //create Areas
-  const analysis = await Area.create({
-    title: 'Analytiscs',
-    introduction: 'Analytics is very cool, you can do lots of stuff! Enjoy us',
-  })
-  const blockchain = await Area.create({
-    title: 'Block Chain',
-    introduction:
-      'Block Chain is very cool, you can do lots of stuff! Enjoy us',
-  })
-  const cloud = await Area.create({
-    title: 'Cloud computation',
-    introduction:
-      'Cloud computation is very cool, you can do lots of stuff! Enjoy us',
-  })
-  const machine = await Area.create({
-    title: 'Machine Learning',
-    introduction:
-      'Machine Learning is very cool, you can do lots of stuff! Enjoy us',
-  })
-
-  // Adding the people to area
-  await analysis.addPerson(person1.id)
-  // Adding the second comment to the first article
-  await cloud.addComment(person2.id)
-}
 /**
  * Function to initialize the database. This is exported and called in the main api.js file
  */
--- server/rest/api.js
+++ server/rest/api.js
@@ -10,61 +10,85 @@ async function init() {
   const db = await initializeDatabase()
 
   // Let's extract all the objects we need to perform queries inside the endpoints
-  const { Area, person, product, use_case } = db._tables
-  // API to get all the areas
+  const { Area, Person, Product, useCase } = db._tables
+
+  //*****   API: get areas   *****//
   app.get('/area', async (req, res) => {
     const area = await Area.findAll()
     return res.json(area)
   })
-  // API to get an area by ID.
   app.get('/area/:id', async (req, res) => {
     const { id } = req.params
     const area = await Area.findOne({
       where: { id },
-      include: { model: Person }, // -> this is the way we "include" also people inside area
-      include: { model: Product }, // -> this is the way we "include" also products inside area
+      include: [{ model: Person }, { model: Product }],
     })
     return res.json(area)
   })
 
-  // API to get all the products
+  //*****   API: get products   *****//
+  // all
   app.get('/product', async (req, res) => {
     const product = await Product.findAll()
     return res.json(product)
   })
-  // API to get an product by ID.
+  // single
   app.get('/product/:id', async (req, res) => {
     const { id } = req.params
     const product = await Product.findOne({
       where: { id },
-      include: { model: Person }, // -> this is the way we "include" also the referent of a product
-      include: { model: Area }, // -> this is the way we "include" also the area of a product
-      include: { model: Use_case }, // -> this include also the use cases of a product
+      include: [{ model: Person }, { model: Area }, { model: useCase }],
+    })
+    return res.json(product)
+  })
+  // by area
+  app.get('/area/product-:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findAll({
+      where: { area_id: id },
+      include: [{ model: Person }, { model: Area }, { model: useCase }],
     })
     return res.json(product)
   })
 
-  // API to get all the people
+  //*****   API: get people   *****//
+  // all
   app.get('/person', async (req, res) => {
     const person = await Person.findAll()
     return res.json(person)
   })
-  // API to get an product by ID.
+  // single
   app.get('/person/:id', async (req, res) => {
     const { id } = req.params
     const person = await Person.findOne({
       where: { id },
-      include: { model: Product }, // -> this include the product which he/she manages
-      include: { model: Area }, // -> this include the area which he/she manages
+      include: [{ model: Product }, { model: Area }],
     })
     return res.json(person)
   })
-  //API to get all the people by area_id
+  // by area_id
+  app.get('/area/:id/people', async (req, res) => {
+    const { id } = req.params
+    const area_People = await Person.findAll({
+      where: { area_id: id },
+    })
+    return res.json(area_People)
+  })
+
+  //*****   API: get use cases of a product   *****//
+  app.get('/product/:id/usecases', async (req, res) => {
+    const { id } = req.params
+    const product_useCases = await useCase.findAll({
+      where: { product_id: id },
+    })
+    return res.json(product_useCases)
+  })
 
   // This one is just an example
   app.get('/ad', (req, res) => {
     return res.json({
-      url: 'https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/styles/simple_image/public/images/media/images/google-display-ads-example-2-final.png?oV7qevVB2XtFyF_O64TG6L27AFM3M2oL&itok=TBfuuTM_',
+      url:
+        'https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/styles/simple_image/public/images/media/images/google-display-ads-example-2-final.png?oV7qevVB2XtFyF_O64TG6L27AFM3M2oL&itok=TBfuuTM_',
     })
   })
 }
