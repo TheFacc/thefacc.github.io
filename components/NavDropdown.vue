db8c85578b345584c6cbeb7de80e3c25f3efca33
TheFacc <imthefacc@gmail.com>
2021-06-07 18:34:41 +0200

    created, working well, mobile friendly

--- components/NavDropdown.vue
+++ components/NavDropdown.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="dropdown-menu-wrapper" @click="isOpen = !isOpen">
-    <div class="dropdown-menu">I am here!</div>
+  <div class="dropdown-menu-wrapper">
+    <div class="dropdown-menu">
+      <li
+        v-for="(item, itemIndex) of items"
+        :key="'menu-item-' + itemIndex"
+        class="dropdown-item"
+      >
+        <nuxt-link :to="item.path">
+          <div v-html="item.icon" class="dropdown-svg" />
+          <span class="dropdown-name">{{ item.name }}</span>
+        </nuxt-link>
+      </li>
+    </div>
   </div>
 </template>
 
@@ -45,7 +56,7 @@ export default {
 <style scoped>
 .dropdown-menu-wrapper {
   /* position: relative;
-  top: 65px;
+  top: 67px;
   width: 500px;
   height: 80px;
   border-radius: 8px;
@@ -77,138 +88,72 @@ export default {
   z-index: 2;
   cursor: pointer;
 }
-.dropdown-menu-wrapper .iconWrapper {
-  width: 25px;
-  height: 25px;
-  position: absolute;
-  right: 30px;
-  top: 50%;
-  transform: translate(0, -50%);
-  z-index: 1;
-}
-.dropdown-menu-wrapper .iconWrapper .bar1 {
-  width: 100%;
-  max-width: 28px;
-  height: 3px;
-  background: blue;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  border-radius: 9999px;
-  transform: translate(-50%, calc(-50% - 8px));
-  transition: all 0.2s ease;
-}
-.dropdown-menu-wrapper .iconWrapper .bar1--open {
-  transform: translate(-50%, -50%) rotate(45deg);
-  margin-top: 0;
-  background: red;
-}
-.dropdown-menu-wrapper .iconWrapper .bar2 {
-  width: 100%;
-  max-width: 28px;
-  height: 3px;
-  background: blue;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  border-radius: 9999px;
-  opacity: 1;
-  transform: translate(-50%, -50%);
-  transition: all 0.2s ease;
-}
-.dropdown-menu-wrapper .iconWrapper .bar2--open {
-  opacity: 0;
-}
-.dropdown-menu-wrapper .iconWrapper .bar3 {
-  width: 100%;
-  max-width: 28px;
-  height: 3px;
-  background: blue;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  border-radius: 9999px;
-  transform: translate(-50%, calc(-50% + 8px));
-  transition: all 0.2s ease;
-}
-.dropdown-menu-wrapper .iconWrapper .bar3--open {
-  top: 50%;
-  transform: translate(-50%, -50%) rotate(-45deg);
-  background: red;
-}
-.dropdown-menu-wrapper .iconWrapper--noTitle {
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  width: auto;
-  height: auto;
-  transform: none;
-}
 .dropdown-menu-wrapper .dropdown-menu {
   position: absolute;
   top: 100%;
-  /* width: 100%;
-  min-width: 300px;
-  min-height: 10px; */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   border-radius: 8px;
   border: 1px solid #eee;
-  box-shadow: 10px 10px 0 0 rgba(0, 0, 0, 0.03);
-  background: white;
-  padding: 10px 30px;
-  animation: menu 0.3s ease forwards;
+  /* box-shadow: 10px 10px 0 0 rgba(0, 0, 0, 0.03); */
+  box-shadow: 0 14px 28px rgb(0 0 0 / 25%), 0 10px 10px rgb(0 0 0 / 22%);
+  background: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(10px);
+  padding: 10px;
 }
-.dropdown-menu-wrapper .dropdown-menu .menuArrow {
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  top: -10px;
-  left: 20px;
-  border-left: 1px solid #eee;
-  border-top: 1px solid #eee;
-  background: white;
-  transform: rotate(45deg);
-  border-radius: 4px 0 0 0;
+@media (min-width: 768px) {
+  .dropdown-menu-wrapper .dropdown-menu {
+    animation: dropdown 0.3s ease;
+  }
 }
-.dropdown-menu-wrapper .dropdown-menu .option {
-  width: 100%;
-  border-bottom: 1px solid #eee;
-  padding: 20px 0;
-  cursor: pointer;
-  position: relative;
-  z-index: 2;
+@media (max-width: 767px) {
+  .dropdown-menu-wrapper .dropdown-menu {
+    animation: dropdownMobile 0.5s ease;
+  }
 }
-.dropdown-menu-wrapper .dropdown-menu .option:last-child {
-  border-bottom: 0;
-}
-.dropdown-menu-wrapper .dropdown-menu .option * {
-  color: inherit;
-  text-decoration: none;
-  background: none;
-  border: 0;
+.dropdown-menu-wrapper .dropdown-menu a {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   padding: 0;
-  outline: none;
-  cursor: pointer;
+  border: 0;
 }
-.dropdown-menu-wrapper .dropdown-menu .desc {
-  opacity: 0.5;
-  display: block;
-  width: 100%;
-  font-size: 14px;
-  margin: 3px 0 0 0;
-  cursor: default;
+.dropdown-menu-wrapper .dropdown-menu .dropdown-svg {
+  margin: 0 10px;
 }
-@keyframes menu {
+/* item */
+.dropdown-menu .dropdown-item a {
+  transition: 0.5s;
+}
+.dropdown-menu .dropdown-item a {
+  border-left: 4px solid transparent;
+}
+.dropdown-menu .dropdown-item:hover a {
+  border-left-color: rgba(0, 0, 0, 0.2);
+  transition: 0s;
+}
+.dropdown-menu .dropdown-item a.nuxt-link-active {
+  border-left-color: rgba(0, 0, 0, 0.85);
+}
+@keyframes dropdown {
   from {
+    opacity: 0;
     transform: translate3d(0, -20px, 0);
   }
   to {
-    transform: translate3d(0, 10px, 0);
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
   }
 }
-.dropdown-menu-wrapper--noTitle {
-  padding: 0;
-  width: 60px;
-  height: 60px;
+@keyframes dropdownMobile {
+  from {
+    opacity: 0;
+    transform: translate(-100px, -65%);
+  }
+  to {
+    opacity: 1;
+    transform: translate(5px, -65%);
+  }
 }
 </style>
