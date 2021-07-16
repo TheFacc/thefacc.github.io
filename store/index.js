f545cf8a2a7acb9f85363820817df2dd97ce0383
TheFacc <imthefacc@gmail.com>
2021-07-16 23:29:56 +0200

    add pageTitle global variable

--- store/index.js
+++ store/index.js
@@ -1,7 +1,11 @@
 export const state = () => ({
+  title: '',
   theme: '#d2d2d2',
 })
 export const mutations = {
+  setTitle(state, value) {
+    state.title = value
+  },
   setTheme(state, value) {
     state.theme = value
   },
