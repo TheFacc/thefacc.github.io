80238b1df7ac8540c2c01b6879928637f70bea48
TheFacc <imthefacc@gmail.com>
2021-07-18 00:34:02 +0200

    slowly trying to improve positioning, hating this

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -8,10 +8,12 @@ export default {}
 
 <style>
 footer {
+  /* position: absolute; */
+  bottom: 0;
+  width: 100%;
+  height: 100px;
   color: white;
   background: #222;
   text-align: center;
-  height: 100px;
-  width: 100%;
 }
 </style>
