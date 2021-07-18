65ed93121116f1ebdc207bd1884af85a3fd24ae4
TheFacc <imthefacc@gmail.com>
2021-07-18 18:50:45 +0200

    page transitions - basic

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,44 @@ export default {
     TheHeader,
     TheFooter,
   },
+  // transition: {
+  //   name: 'pagefar',
+  //   mode: '', // simultaneous? nope =(
+  // },
+  // transition(to, from) {
+  //   if (!from) {
+  //     return 'pagefar'
+  //   }
+  //   return +to.path.split('/').length < +from.path.split('/').length ? 'pagenear' : 'pagefar'
+  // },
 }
 </script>
 
-<style></style>
+<style>
+/* PAGE TRANSITIONS */
+/* between different pages ('bigger mind jump') */
+.page-enter-active,
+.page-leave-active {
+  transition: 0.2s ease;
+}
+.page-enter {
+  opacity: 0;
+  transform: translateX(20px);
+  transform-origin: top;
+}
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(-20px);
+  transform-origin: top;
+}
+/* between similar pages (between areas, between related products) */
+.pagenear-enter-active,
+.pagenear-leave-active {
+  transition: 1s ease;
+}
+.pagenear-enter,
+.pagenear-leave-to {
+  opacity: 0.8;
+  filter: blur(50px);
+}
+</style>
