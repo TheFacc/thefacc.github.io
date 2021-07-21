3895a1a3d2ff7fc06a4385245e69f14e05bd13f8
TheFacc <imthefacc@gmail.com>
2021-07-21 18:55:38 +0200

    init contact page/component

---
+++ components/ContactForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="form">
+    <div class="fields">
+      <input v-model="name" type="text" />
+      <input v-model="surname" type="text" />
+      <input v-model="phone" type="text" />
+      <textarea v-model="content" cols="30" rows="10"></textarea>
+    </div>
+    <div class="submit">
+      <button @click="createPost()">Create</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      surname: '',
+      phone: '',
+      content: '',
+    }
+  },
+  methods: {
+    createPost() {
+      const data = {
+        name: this.name,
+        surname: this.surname,
+        phone: this.phone,
+        content: this.content,
+      }
+      this.$axios.post('/api', data)
+      this.name = ''
+      this.surname = ''
+      this.phone = ''
+      this.content = ''
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: column;
+}
+.fields {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: column;
+}
+
+p {
+  text-align: left;
+}
+</style>
---
+++ pages/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="container">
+    <h1>Testpage</h1>
+    <section>
+      <p>Company locations, number, etc</p>
+    </section>
+    <contact-form></contact-form>
+  </main>
+</template>
+
+<script>
+import ContactForm from '~/components/ContactForm.vue'
+export default {
+  component: { ContactForm },
+  mounted() {
+    this.$store.commit('setTheme', '#3f51b5')
+    this.$store.commit('setTitle', '')
+    this.$store.commit('updateRoute', {
+      title: 'Contact page',
+      url: window.location.href,
+    })
+  },
+}
+</script>
+
+<style scoped>
+p {
+  text-align: left;
+}
+</style>
