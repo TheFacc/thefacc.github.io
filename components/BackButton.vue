caa5a5d823e7af38a247067b45ed034ec80fee46
TheFacc <imthefacc@gmail.com>
2021-07-21 18:54:01 +0200

    generalized back button

---
+++ components/BackButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    :tooltip="text ? text : 'Go back'"
+    flow="right"
+    class="goback"
+    :class="{ bounce: animated }"
+    @click="
+      animated = true
+      path ? $goTo(path) : $goBack()
+    "
+    @animationend="animated = false"
+  >
+    <img :src="require('~/assets/icons/back.svg')" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { type: String, default: '' },
+    path: { type: String, default: '' },
+  },
+  data() {
+    return {
+      animated: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.goback {
+  position: absolute;
+  display: flex;
+  padding: 20px;
+  width: 60px;
+  height: 60px;
+  top: 77px;
+  left: 10px;
+  font-size: 32px;
+  text-align: center;
+  color: white;
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(3px);
+  border-radius: 100%;
+  transition: 0.2s;
+}
+.goback:hover {
+  cursor: pointer;
+  transform: scale(1.05);
+}
+.goback img {
+  filter: invert(1);
+}
+</style>
