ba5e18b6e7c5fa31ebc8665b8e0c85c130765354
Mousasan97 <78212372+Mousasan97@users.noreply.github.com>
2021-07-26 09:12:09 +0200

    Chatbot edit spelling errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     // BASE_URL: '', // Development URL
     API_URL: 'https://moube.herokuapp.com', // fetch() always needs absolute url
     GOOGLE_MAPS_API_KEY: 'AIzaSyC8RoIzwgLliflh7Vb5-PaCiOtd4rGKg50', // for the contact page only
-    configurationId: 'fd1b9b11-407a-46c3-a78c-eb806503e2db', // chatbot access
+    configurationId: '56acaecf-5f6a-45e9-8cb8-c34a7f4975ae', // chatbot access
   },
   serverMiddleware: [
     {
