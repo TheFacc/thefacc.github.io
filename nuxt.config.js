13786c3e0b806d8284a8daf1cdda9df49c7991b7
Mousasan97 <78212372+Mousasan97@users.noreply.github.com>
2021-07-26 09:58:10 +0200

    edits chatbot

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     // BASE_URL: '', // Development URL
     API_URL: 'https://moube.herokuapp.com', // fetch() always needs absolute url
     GOOGLE_MAPS_API_KEY: 'AIzaSyC8RoIzwgLliflh7Vb5-PaCiOtd4rGKg50', // for the contact page only
-    configurationId: 'd7e4b85b-fefe-4c92-8dec-7eec2846004e', // chatbot access
+    configurationId: 'c8af720b-a988-48da-87b7-d765044ad624', // chatbot access
   },
   serverMiddleware: [
     {
